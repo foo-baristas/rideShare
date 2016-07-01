074bbbe34f61287c58676afaf136d1d5457a73e3
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-07-01 15:42:41 -0700

    fixed passenger migration

--- migrations/20160701153822_passengers.js
+++ migrations/20160701153822_passengers.js
@@ -3,6 +3,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('passengers', function (table) {
     table.increments();
+    table.integer('trip_id').notNullable();
     table.integer('user_id').notNullable();
   });
 };
