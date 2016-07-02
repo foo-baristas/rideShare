3b4c155526d3f1a3e46116c069cb6ddfae7c4957
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-07-02 10:54:37 -0700

    created knex objects for post and put

--- routes/users.js
+++ routes/users.js
@@ -43,7 +43,7 @@ router.post('/', function(req, res) {
 
   //  CONTINUE TO FILL IN INSERT OBJECT
 
-  knex('users').insert({name_first: req.body.user.name, }).then(function(data){
+  knex('users').insert({name_first: req.body.user.name_first, name_last: req.body.user.name_last, profile_pic_url: req.body.user.profile_pic_url, age: req.body.user.age, description: req.body.user.description, email: req.body.user.email, username: req.body.user.username, password: req.body.user.password, preferences_id: req.body.user.preferences_id, is_driver: req.body.user.is_driver, isFB_verified: req.body.user.isFB_verified}).then(function(data){
     res.redirect('/user/' + req.params.id);
   }).catch(function(err){
     console.error(err);
@@ -55,7 +55,7 @@ router.put('/:id', function(req, res) {
 
   // COPY INSERT OBJECT FROM ABOVE HERE
 
-  knex('users').update({name_first: req.body.user.name}).where({id: req.params.id}).then(function(data){
+  knex('users').update({name_first: req.body.user.name_first, name_last: req.body.user.name_last, profile_pic_url: req.body.user.profile_pic_url, age: req.body.user.age, description: req.body.user.description, email: req.body.user.email, username: req.body.user.username, password: req.body.user.password, preferences_id: req.body.user.preferences_id, is_driver: req.body.user.is_driver, isFB_verified: req.body.user.isFB_verified}).where({id: req.params.id}).then(function(data){
     res.redirect('/user/' + req.params.id);
   }).catch(function(err){
     console.error(err);
