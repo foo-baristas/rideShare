bfe6d3b5bdc5ea67fcb9970325ec44268ec4bc45
Jeremy <jkaffrey@gmail.com>
2016-07-02 16:41:09 -0600

    Created post request to reserve seat, currently it only decreases the number of seats, it does not assign a rider

--- routes/search.js
+++ routes/search.js
@@ -3,12 +3,14 @@ var express = require('express'),
     router = express.Router(),
     knex = require('../db/knex');
 
+//search?destination=denver&origin=fortcollins
 router.get('/', function(req, res, next) {
 
   var query = req.query;
   console.log(query);
   knex('trips')
   .join('users', 'users.id', '=', 'trips.user_id')
+  .join('preferences', 'preferences.id', '=', 'trips.preferences_id')
   .select().then(function(data) {
 
     console.log(data);
@@ -17,7 +19,13 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/reserve/:id', function(req, res, next) {
-  
+
+  knex('trips')
+  .where('id', '=', req.params.id)
+  .decrement('num_seats', 1)
+  .then(function(data) {
+    res.redirect('/search');
+  });
 });
 
 module.exports = router;
