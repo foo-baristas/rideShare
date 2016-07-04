f0556bb3dfbb0555c338f7b43bf39fafba8c0364
Jeremy <jkaffrey@gmail.com>
2016-07-03 20:47:43 -0600

    Fixed issue preventing app from running

--- routes/index.js
+++ routes/index.js
@@ -45,16 +45,15 @@ function checkRequired(req, info) {
   for (var item in req.body) {
     info[item] = req.body[item];
 
-        if (req.body[item].length <= 0) {
-            // TODO: ask how this code works (wouldn't work unless this existed, taken out of the previous if would print empty arrays in error object)
-            if (!info.error[item]) {
-                info.error[item] = [];
-            }
-            info.hasError = true;
-            info.error[item].push(
-                item + " is required."
-            );
-        }
+    if (req.body[item].length <= 0) {
+      // TODO: ask how this code works (wouldn't work unless this existed, taken out of the previous if would print empty arrays in error object)
+      if (!info.error[item]) {
+        info.error[item] = [];
+      }
+      info.hasError = true;
+      info.error[item].push(
+        item + ' is required.'
+      );
     }
   }
 }
@@ -84,15 +83,15 @@ router.post('/auth', function(req, res, next) {
       //   }
       // });
       if (req.body.password === data[0].password) {
-          //TODO: change to render landing page with greeting + user's name (dynamically update in header partial)
-          req.session = {};
-          req.session.user_id = data[0].id;
-          req.session.user_name = data[0].username;
-          console.log(req.session);
-          res.redirect('/index');
+        //TODO: change to render landing page with greeting + user's name (dynamically update in header partial)
+        req.session = {};
+        req.session.user_id = data[0].id;
+        req.session.user_name = data[0].username;
+        console.log(req.session);
+        res.redirect('/index');
       } else {
-          //TODO: output on same page with same message in error format
-          res.send('Sorry, username and password don\'t match.');
+        //TODO: output on same page with same message in error format
+        res.send('Sorry, username and password don\'t match.');
       }
     } //TODO: output on same page with same message in error format
     else {
