4b3ace5f7ac5e35ae02c0b18d6b12ad89f8a8c77
Jeremy <jkaffrey@gmail.com>
2016-07-05 11:10:39 -0600

    Setting up deployment

--- knexfile.js
+++ knexfile.js
@@ -7,5 +7,26 @@ module.exports = {
       min: 1,
       max: 1
     }
+  },
+
+  test: {
+    client: 'postgres',
+    connection: 'postgres://postgres@localhost/ride-share',
+    pool: {
+      min: 1,
+      max: 1
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
 };
